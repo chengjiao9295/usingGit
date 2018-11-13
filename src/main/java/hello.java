cff625222e945f1034c5e695d957d7027dd0e5e2
chengjiao9295 <38353363+chengjiao9295@users.noreply.github.com>
2018-11-13 22:02:42 +0800

    Update hello.java

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     //url https://github.com/chengjiao9295/usingGit.git
     public static void main(String []args){
         System.out.print("aaaaa");
+        System.out.print("bbbbb");
     }
 }

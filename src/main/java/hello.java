acce5b70727a82e13aa7467611fb292d9d03e1ef
Chengjiao <735813549@qq.com>
2018-11-14 21:40:17 +0800

    usingGit third commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,5 +3,11 @@ public class hello {
     public static void main(String []args){
         System.out.print("aaaaa");
         System.out.print("bbbbb");
+        System.out.print("ccccc");
+        System.out.print("ddddd");
+        System.out.print("eeeewww");
+        //
+        //ddddddddddd
+        //sssss
     }
 }
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,6 @@
 public class test {
 
     public static void main(String []args){
-        System.out.print("aaaaa");
+        System.out.print("wwwcccc");
     }
 }

04aefe712b9e1782d650084eedde5ef4af9b1b3b
chengjiao9295 <38353363+chengjiao9295@users.noreply.github.com>
2018-12-31 15:30:11 +0800

    Update hello.java

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -13,6 +13,7 @@ public class hello {
         System.out.print("merge into master;now the branch is on the newBran");
         System.out.print("given that someone else hava commited a new code before you commit");
         System.out.print("you also modified the code");
-         System.out.print("given that someone else hava commited a new code before you commit");
+        System.out.print("given that someone else hava commited a new code before you commit");
+        System.out.print("given that someone else hava commited a new code before you commit");
     }
 }

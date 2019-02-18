8e0f446e1678024b3a0561dfb4e76ac06259696d
Chengjiao <735813549@qq.com>
2019-02-18 23:46:33 +0800

    modified client.java file

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,17 +1,72 @@
-import org.junit.Test;
-
 import java.lang.reflect.*;
+
 import java.util.Map;
 
+import org.junit.Test;
+
+/**
+ *
+ */
 public class Client {
     private Map<String, Object> objectMap;
 
-    public void test(Map<String, User> map, String string) {
+    /**
+     * Method description
+     *
+     *
+     * @throws Exception
+     */
+    @Test
+    public void client() throws Exception {
+        ObjectPool pool = ObjectPool.init("config.json");
+        User       user = (User) pool.getObject("id1");
+
+        System.out.println(user);
+
+        Bean bean = (Bean) pool.getObject("id2");
+
+        System.out.println(bean);
+
+        ComplexBean complexBean = (ComplexBean) pool.getObject("id3");
+
+        System.out.println(complexBean);
+
+        User  user1   = new User();
+        Field idFiled = User.class.getDeclaredField("id");
+
+        setAccessible(idFiled);
+        idFiled.setInt(user1, 46);
+
+        Field nameFiled = User.class.getDeclaredField("name");
+
+        setAccessible(nameFiled);
+        nameFiled.set(user1, "feiqing");
+
+        Field passwordField = User.class.getDeclaredField("password");
+
+        setAccessible(passwordField);
+        passwordField.set(user1, "ICy5YqxZB1uWSwcVLSNLcA==");
+        System.out.println(user1);
     }
 
+    /**
+     * Method description
+     *
+     *
+     * @return
+     */
     public Map<User, Bean> test() {
         return null;
     }
+
+    /**
+     * Method description
+     *
+     *
+     * @param map
+     * @param string
+     */
+    public void test(Map<String, User> map, String string) {}
 
     /**
      * 测试属性类型
@@ -21,14 +76,17 @@ public class Client {
     @Test
     public void testFieldType() throws NoSuchFieldException {
         Field field = Client.class.getDeclaredField("objectMap");
-        Type gType = field.getGenericType();
+        Type  gType = field.getGenericType();
+
         // 打印type与generic type的区别
         System.out.println(field.getType());
         System.out.println(gType);
         System.out.println("**************");
+
         if (gType instanceof ParameterizedType) {
             ParameterizedType pType = (ParameterizedType) gType;
-            Type[] types = pType.getActualTypeArguments();
+            Type[]            types = pType.getActualTypeArguments();
+
             for (Type type : types) {
                 System.out.println(type.toString());
             }
@@ -42,12 +100,15 @@ public class Client {
      */
     @Test
     public void testParamType() throws NoSuchMethodException {
-        Method testMethod = Client.class.getMethod("test", Map.class, String.class);
+        Method testMethod     = Client.class.getMethod("test", Map.class, String.class);
         Type[] parameterTypes = testMethod.getGenericParameterTypes();
+
         for (Type type : parameterTypes) {
             System.out.println("type -> " + type);
+
             if (type instanceof ParameterizedType) {
                 Type[] actualTypes = ((ParameterizedType) type).getActualTypeArguments();
+
                 for (Type actualType : actualTypes) {
                     System.out.println("\tactual type -> " + actualType);
                 }
@@ -63,45 +124,23 @@ public class Client {
     @Test
     public void testReturnType() throws NoSuchMethodException {
         Method testMethod = Client.class.getMethod("test");
-        Type returnType = testMethod.getGenericReturnType();
+        Type   returnType = testMethod.getGenericReturnType();
+
         System.out.println("return type -> " + returnType);
 
         if (returnType instanceof ParameterizedType) {
             Type[] actualTypes = ((ParameterizedType) returnType).getActualTypeArguments();
+
             for (Type actualType : actualTypes) {
                 System.out.println("\tactual type -> " + actualType);
             }
         }
     }
 
-    @Test
-    public void client() throws Exception{
-        ObjectPool pool = ObjectPool.init("config.json");
-        User user = (User) pool.getObject("id1");
-        System.out.println(user);
-
-        Bean bean = (Bean) pool.getObject("id2");
-        System.out.println(bean);
-
-        ComplexBean complexBean = (ComplexBean) pool.getObject("id3");
-        System.out.println(complexBean);
-        User user1 = new User();
-        Field idFiled = User.class.getDeclaredField("id");
-        setAccessible(idFiled);
-        idFiled.setInt(user1, 46);
-
-        Field nameFiled = User.class.getDeclaredField("name");
-        setAccessible(nameFiled);
-        nameFiled.set(user1, "feiqing");
-
-        Field passwordField = User.class.getDeclaredField("password");
-        setAccessible(passwordField);
-        passwordField.set(user1, "ICy5YqxZB1uWSwcVLSNLcA==");
-
-        System.out.println(user1);
-    }
-
     private void setAccessible(AccessibleObject object) {
         object.setAccessible(true);
     }
 }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
